2d25a7515cce71aaa576e1dc0660a17e65a610be
Mylon Suren <mylonsuren@gmail.com>
2022-04-08 13:45:03 -0400

    [MM-42739] Initial setup for top channels for team

--- app/channel.go
+++ app/channel.go
@@ -3395,3 +3395,15 @@ func (a *App) getDirectChannel(userID, otherUserID string) (*model.Channel, *mod
 
 	return channel, nil
 }
+
+func (a *App) GetTopChannelsForTeamSince(teamID, userID string, opts *model.InsightsOpts) (*model.TopChannelList, *model.AppError) {
+	if !a.Config().FeatureFlags.InsightsEnabled {
+		return nil, model.NewAppError("GetTopChannelsForTeamSince", "api.insights.feature_disabled", nil, "", http.StatusNotImplemented)
+	}
+
+	topChannels, err := a.Srv().Store.Channel().GetTopChannelsForTeamSince(teamID, userID, opts.StartUnixMilli, opts.Page*opts.PerPage, opts.PerPage)
+	if err != nil {
+		return nil, model.NewAppError("GetTopChannelsForTeamSince", "app.reaction.get_top_for_team_since.app_error", nil, err.Error(), http.StatusInternalServerError)
+	}
+	return topChannels, nil
+}
